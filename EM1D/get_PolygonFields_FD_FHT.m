affdac0f71bb42c89316ee7446bab748b6cfb05d
kerrykey <kkey@ucsd.edu>
2017-01-24 15:43:12 -0500

    slight changes to input variables for get_PolygonFields_FD_FHT so that they're easier to understand. Also note range there is the horizontal range so we ignore the z coordinate.

--- EM1D/get_PolygonFields_FD_FHT.m
+++ EM1D/get_PolygonFields_FD_FHT.m
@@ -1,4 +1,4 @@
-function Bz = get_PolygonFields_FD_FHT(freqs,zTx,rRx,zRx,sig,mu,z,filterName,vertices,GQorder,A)
+function Bz = get_PolygonFields_FD_FHT(freqs,xyPolyTx,zTx,xyRx,zRx,sig,mu,z,filterName,GQorder)
 
 %This function calculates the frequency domain magnetic field of a polygon
 %shaped loop of wire. It does so by approximating the loop as a set of
@@ -11,15 +11,15 @@ function Bz = get_PolygonFields_FD_FHT(freqs,zTx,rRx,zRx,sig,mu,z,filterName,ver
 %uses Gauss quadrature using get_VMD_FD_FHT at those points with those
 %weights.
 
-%receiver location is the last row of vertices
-r = vertices(end,:);
+ 
 %compute center of polygon defined by vertices
-centerPoly = mean(vertices);
+centerPoly = mean(xyPolyTx);
+
 %now each pair of neighboring vertices on the polygon has a third point
 %(the center) - forming a triangle
 
-%number of polygon vertices + 1 (the receiver location is included last)
-nvertices = size(vertices,1);
+%number of polygon vertices 
+nvertices = size(xyPolyTx,1);
 
 nf = length(freqs);
 %initialize the value of the integral to zero
@@ -27,53 +27,73 @@ nf = length(freqs);
 I = zeros(1,nf);
 %method 2
 I2 = I;
+
+%kwk debug: plot triangles:
+figure;
+
+
 %loop over the number of triangles = the number of polygon vertices
-for k=1:nvertices-1
+for k=1:nvertices
     %find the 3 vertices of the current triangle (2 polygon + center)
-    if (k == nvertices-1)
-        v = [ vertices(k,:) ; vertices(1,:) ; centerPoly ];
+    if (k == nvertices)
+        v = [ xyPolyTx(k,:) ; xyPolyTx(1,:) ; centerPoly ];
     else
-        v = [ vertices(k,:) ; vertices(k+1,:) ; centerPoly ];
+        v = [ xyPolyTx(k,:) ; xyPolyTx(k+1,:) ; centerPoly ];
     end
+    
+
+     
     %compute the Gauss quadrature points and weights for this triangle
     %method 1
     [X,Y,Wx,Wy]=triquad(GQorder,v);
     %method 2
     [x,w] = simplexquad(GQorder,v);
 
-    %compute distance from each integration point to the receiver (matrix)
-    Rx = ones(GQorder)*r(1,1);
-    Ry = ones(GQorder)*r(1,2);
-    Rz = ones(GQorder)*(zRx - zTx);
-    R = ((X-Rx).^2 + (Y-Ry).^2 + (Rz).^2).^(0.5);
+    %kwk debug: plot triangles and quad points:    
+     plot(v([1:end 1],1),v([1:end 1],2),'k-'); hold on
+     plot(x(:,1),x(:,2),'o'); hold on
+      plot(X,Y,'+'); hold on
+  
+    
+% KWK debug R is just horizontal range to receiver!
+% Rz = ones(GQorder)*(zRx - zTx); 
+   % R = sqrt((X-xyRx(1)).^2 + (Y-xyRx(2)).^2);  %+ (Rz).^2).^(0.5);
+    
     %compute vector of distances (for I2)
-    distRx = ((x(:,1)-ones(size(x,1),1)*r(1,1)).^2 + ...
-        (x(:,2)-ones(size(x,1),1)*r(1,2)).^2 + ...
-        ones(size(x,1),1)*(zRx - zTx)^2).^(0.5);
-
+    distRx = sqrt( (x(:,1) - xyRx(1)).^2 + ...
+                   (x(:,2) - xyRx(2)).^2 ); 
+ 
+    
     %evaluate the vertical magnetic dipole at each integration point
     %method 2
     for l=1:length(distRx)
         I2 = I2 + get_VMD_FD_FHT(freqs,zTx,distRx(l),zRx,sig,mu,z,filterName)*w(l);
     end
     %method 1
-    feval = zeros(size(R,1),size(R,2),nf);
-    for l=1:size(R,1)
-        for m=1:size(R,2)
-            feval(l,m,:) = get_VMD_FD_FHT(freqs,zTx,R(l,m),zRx,sig,mu,z,filterName);
-        end
-    end
-    %compute the integral over this triangle (finally!)
-    for j=1:nf
-        I(j) = I(j) + Wx'*feval(:,:,j)*Wy;
-    end
+%     feval = zeros(size(R,1),size(R,2),nf);
+%     for l=1:size(R,1)
+%         for m=1:size(R,2)
+%             feval(l,m,:) = get_VMD_FD_FHT(freqs,zTx,R(l,m),zRx,sig,mu,z,filterName);
+%         end
+%     end
+%     %compute the integral over this triangle (finally!)
+%     for j=1:nf
+%         I(j) = I(j) + Wx'*feval(:,:,j)*Wy;
+%     end
     %on to the next triangle!
 end
-%normalize by the area of the loop
-I = I/A;
-I2 = I2/A;
-BzCircle = get_VMD_FD_FHT(freqs,zTx,rRx,zRx,sig,mu,z,filterName);
+
+% normalize by true polygon area:
+area = polyarea(xyPolyTx(:,1),xyPolyTx(:,2));
+
+%I = I/area;
+I2 = I2/area;
+% BzCircle = get_VMD_FD_FHT(freqs,zTx,rRx,zRx,sig,mu,z,filterName);
 Bz = I2;
+ 
+
+
+
 %keyboard
 
 
--- EM1D/testing_script.m
+++ EM1D/testing_script.m
@@ -2,32 +2,69 @@
 clear all
 clc
 
-nFreqsPerDecade = 10;
+nFreqsPerDecade     = 10;
 freqLowLimit        = 1d-3; 
 freqHighLimit       = 1d6;
 freqs = 10.^(log10(freqLowLimit):1/nFreqsPerDecade:log10(freqHighLimit));
 
-z   = [-1d5     0   100  ];   
-sig = [1d-20   1/10 1/10 ];
+z   = [-1d5    0   100  ];   
+sig = [1d-12   1/10 1/10 ];
 mu = ones(size(sig));
 
-zTx   = -30; 
-rRx   = 2+sqrt(488/pi);
-zRx = -31.90;
-A = 488; %area of polygon in square meters
+zTx     = -30; 
+zRx     = -31.90;
+xyRx    = [-17 0]; 
 
-HankelFilterName = 'kk201Hankel.txt';
+xyPolyTx = [ -15.09 -2.00 ; -8.11 -10.16 ; 8.11 -10.16 ; 15.09 -2.00 ; 15.09 2.00 ;...
+               8.11 10.16 ; -8.11  10.16 ; -15.09 2.00 ];
+           
+HankelFilterName = 'kk201Hankel.txt';    
+GQorder        = 3;
 
-%the last vertex is actually the receiver location
-vertices = [ -15.09 -2.00 ; -8.11 -10.16 ; 8.11 -10.16 ; 15.09 -2.00 ; 15.09 2.00 ;...
-        8.11 10.16 ; -8.11 10.16 ; -15.09 2.00 ; -17.00 0.00 ];
-    
-GQorder = 2;
 
-%Bz = get_VMD_FD_FHT(freqs,zTx,rRx,zRx,sig,mu,z,HankelFilterName);
+% For testing far field solution, make the transmitter polygon tiny so that
+% it looks like a point dipole at the receiver location:
+xyPolyTx    = xyPolyTx/100;    
+% Then the fields should be nearly the same (<< 1% difference). 
+
+% Polyon transmitter:
+tic
+BzFDPoly = get_PolygonFields_FD_FHT(freqs,xyPolyTx,zTx,xyRx,zRx,sig,mu,z,HankelFilterName,GQorder);
+t = toc;
+fprintf('%32s %.3f s\n','get_PolygonFields_FD_FHT time:',t);
+
+% Perfectly circular loop:  
+area     = polyarea(xyPolyTx(:,1),xyPolyTx(:,2));   % circle approximation requires area of polygon
+rTxLoop  = sqrt(area/pi);                           % from area = pi*r^2
+rRx      = norm(xyRx);                              % receiver is at same range from center of polygon 
 
 tic
-BzFD = get_PolygonFields_FD_FHT(freqs,zTx,rRx,zRx,sig,mu,z,HankelFilterName,vertices,GQorder,A);
-toc
+BzFDCirc = get_LoopFields_FD_FHT(freqs,rTxLoop,zTx,rRx,zRx,sig,mu,z,HankelFilterName);
+t = toc;
+fprintf('%32s %.3f s\n','get_LoopFields_FD_FHT time:',t);
 
-%norm(Bz-BzFD)
+% Point dipole:
+tic
+BzPoint = get_VMD_FD_FHT(freqs,zTx,rRx,zRx,sig,mu,z,HankelFilterName);
+t = toc;
+fprintf('%32s %.3f s\n','get_VMD_FD_FHT time:',t);
+% Note that when rRx >> rTxLoop, then the point dipole field and loop and
+% polygon fields should look the same
+
+figure;
+subplot(2,1,1)
+loglog(freqs,abs(BzFDPoly),'b-',freqs,abs(BzFDCirc),'ro',freqs,abs(BzPoint),'g+')
+legend('Polygon','Circle','Point')
+xlabel('Frequency (Hz)')
+ylabel('Amplitude')
+
+subplot(2,1,2)
+loglog(freqs,abs(BzFDCirc-BzFDPoly)./abs(BzFDCirc)*100,'b-')
+hold on;
+loglog(freqs,abs(BzFDCirc-BzPoint)./abs(BzPoint)*100,'r-')
+loglog(freqs,abs(BzFDPoly-BzPoint)./abs(BzPoint)*100,'g-')
+xlabel('Frequency (Hz)')
+ylabel('Relative Difference (%)')
+legend('(Polygon-Circle)/Circle','(Circle-Point)/Point','(BzFDPoly-Point)/Point')
+
+ 
