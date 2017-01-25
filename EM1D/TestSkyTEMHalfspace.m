17389e43d9d0b310daba806f79f0fa99e12cd774
kerrykey <kkey@ucsd.edu>
2017-01-24 20:16:02 -0500

    -Fixed SkyTEM area normalization to be true loop area -updated code at bottom that compares polygon code with central loop analytic solution.

--- EM1D/TestSkyTEMHalfspace.m
+++ EM1D/TestSkyTEMHalfspace.m
@@ -119,7 +119,7 @@ dLM=[ 5.738414E-06
  1.144417E-08
  6.789652E-09
  3.946301E-09
-]/494;
+];
 
 dHM = [
 8.457780E-07
@@ -141,7 +141,7 @@ dHM = [
 2.096507E-10
 1.181879E-10
 6.198442E-11
-]/494;
+];
 
 
 
@@ -161,6 +161,10 @@ xyRx    = [17 0];
 zRxLM = -31.90;
 zRxHM = -30.19;         % m, reciever coil vertical position (use same datum as model z).
  
+% Normalize SkyTEM solutions by loop area:
+area = polyarea(xyPolyTx(1:end,1),xyPolyTx(1:end,2) );
+dHM = dHM/area;
+dLM = dLM/area;
 
 %--------------------------------------------------------------------------
 %-------- You shouldn't need to change anything below here ----------------
@@ -168,10 +172,11 @@ zRxHM = -30.19;         % m, reciever coil vertical position (use same datum as 
 %
 % Some numerial parameters used by the forward code (don't change these):
 %
-nFreqsPerDecade = 10;
-LoopQuadOrder   = 1;   % Gauss quadrature order for polygon loop integration
+nFreqsPerDecade = 15;
+LoopQuadOrder   = 3;   % Gauss quadrature order for polygon loop integration
 HankelFilterName = 'kk201Hankel.txt';
 CosSinFilterName = 'kk201CosSin.txt';
+
 mu = ones(size(sig)); % relative magnetic permeability of each layer. Set this to 1 always!
 
 % 
@@ -222,41 +227,68 @@ legend('LM', 'HM')
  
  
 %% Compare with Analytical formula central loop sounding on surface of a halfspace:
+clear all
 
-%  
-%  % % % formula from Christiansen et el groundwater chapter 
-% % % %eqn 6.26:
-% 
-% HankelFilterName = 'wa801Hankel.txt';
-% CosSinFilterName = 'kk201CosSin.txt';
-% 
-% % For testing with halfpsace equation:
-% 
-% rTxLM = 10; % radius of loop. 
-% zRxLM = 0; % Rx depth. has to be 0 since Christiansen formula (from Ward and Hohmann actually) is valid only on surface of halspace
-% zTxLM = 0; % Tx depth. has to be 0 since Christiansen formula (from Ward and Hohmann actually) is valid only on surface of halspace
-%  
-% sig0 = 1/10;
-% rRxLM = 0;  % has to be 0 since Christiansen formula is only for receiver coil in center of transmitter loop
-% 
-% nFreqsPerDecade = 10;
-% tlong = logspace(-6,-1,100);
-% 
-% theta = sqrt(4*pi*1d-7*sig0 ./ (4*tlong) );
-% a = rTxLM;  
-% dbzdt = -1/(sig0*a^3)*(3*erf(theta*a) - 2/sqrt(pi)*theta.*a.*(3+2*theta.^2*a^2).*exp(-theta.^2*a^2));
-% dbzdt = dbzdt/(pi*a^2);
-%  
-% BzLMlong = get_LoopFields_TD_FHT(tlong,rTxLM,zTxLM,rRxLM,zRxLM,sig,mu,z, HankelFilterName,CosSinFilterName,nFreqsPerDecade);   
-% 
-% 
-% figure;
-% subplot(2,1,1)
-% loglog(tlong,-(dbzdt),'k-','linewidth',1);
-% hold on;
-% loglog(tlong,-(BzLMlong),'r--','linewidth',1);
-% legend('Analytic','Numerical')
-% 
-% subplot(2,1,2)
-% loglog(tlong,100*abs(dbzdt'-BzLMlong)./abs(dbzdt)','k-','linewidth',1);
+% For testing with halfpsace equation:
+
+% % % formula from Christiansen et el groundwater chapter 
+% % %eqn 6.26:
+
+
+nFreqsPerDecade = 15;
+LoopQuadOrder   = 1;   % Gauss quadrature order for polygon loop integration
+loopRadius      = 15; % 10 m circular loop for Christiansen formula
+
+nPolygonSides   = 30;  % number of polygon sides. more means better circle approximation. must be >2
+
+HankelFilterName = 'kk201Hankel.txt';
+CosSinFilterName = 'kk201CosSin.txt';
+
+z   = [-1d5     0     ];   % m, vertical position of layer top boundaries, 1st one ignored since it's the top of air.
+sig = [1d-20   1/10   ]; 
+
+
+%-------------------------------
+% Don't change anything below here:
+
+tlong       = logspace(-6,-1,40);
+xyRx        = [0 0];  % has to be 0 since Christiansen formula is only for receiver coil in center of transmitter loop
+zRx         = 0; % Rx depth. has to be 0 since Christiansen formula (from Ward and Hohmann actually) is valid only on surface of halspace
+zTx         = 0; % Tx depth. has to be 0 since Christiansen formula (from Ward and Hohmann actually) is valid only on surface of halspace
+mu          = ones(size(sig)); % relative magnetic permeability of each layer. Set this to 1 always!
+
+% Make a polygon approximation of a circle to test get_PolygonFields versus
+% Christiansen et al analytic formula for perfect circle
  
+L = -pi/2+linspace(0,2*pi,nPolygonSides+1);
+L = L(1:end-1);
+xyPolyTx(:,1) = loopRadius*cos(L)';
+xyPolyTx(:,2) = loopRadius*sin(L)';
+ 
+% Nothing to change below here:
+sig0 = sig(2);
+theta = sqrt(4*pi*1d-7*sig0 ./ (4*tlong) );
+a     = loopRadius;  
+dbzdt = -1/(sig0*a^3)*(3*erf(theta*a) - 2/sqrt(pi)*theta.*a.*(3+2*theta.^2*a^2).*exp(-theta.^2*a^2));
+dbzdt = dbzdt/(pi*a^2);
+
+
+% Compute loop response for the polygon, without any waveform ramp or
+% filters:
+BzPoly = get_LoopFields_TD_FHT(tlong,xyPolyTx,zTx,xyRx,zRx,sig,mu,z, HankelFilterName,CosSinFilterName,nFreqsPerDecade,...
+                             LoopQuadOrder);    
+
+
+figure;
+subplot(2,1,1)
+loglog(tlong,-(dbzdt),'k-','linewidth',1);
+hold on;
+loglog(tlong,-(BzPoly),'r--','linewidth',1);
+legend('Analytic','Polygon')
+xlabel('Time (s)')
+title(sprintf('LoopQuadOrder=%i, nPolygonSides=%i, nFreqsPerDecade=%i',LoopQuadOrder,nPolygonSides,nFreqsPerDecade))
+
+subplot(2,1,2)
+loglog(tlong,100*abs(dbzdt'-BzPoly)./abs(dbzdt)','k-','linewidth',1);
+ylabel('Relative Difference (%)')
+xlabel('Time (s)')
