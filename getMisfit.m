a1be0160bbea7ba096a2a72fcaaa777ac89ba966
Ray Anandaroop <Anandaroop.Ray@ga.gov.au>
2020-01-21 10:45:07 +1100

    getMisfit now computes RMS error if sd supplied, properly

--- getMisfit.m
+++ getMisfit.m
@@ -2,7 +2,7 @@
 %Large Loop misfit function
 function misfit = getMisfit(x,S)
 
-if S.debug_prior ~= true
+if ~S.debug_prior
     
     % Get Bz for loop source:
     Bz = get_field(S,x);
@@ -11,20 +11,24 @@ if S.debug_prior ~= true
     idx_HM = ~isnan(S.d_HM);
     n_LM = sum(idx_LM);
     n_HM = sum(idx_HM);
-    r = (abs([S.d_LM(idx_LM); S.d_HM(idx_HM)])...
-       - abs(Bz([idx_LM; idx_HM])))./abs([S.d_LM(idx_LM); S.d_HM(idx_HM)]);
+    r_LM = abs(S.d_LM(idx_LM)) - abs(Bz(idx_LM));
+    r_HM = abs(S.d_HM(idx_HM)) - abs(Bz(idx_HM));
     
-    n = n_LM + n_HM;
-    chi2by2 = r'*r/2;
-    rms = 2*chi2by2/n;
-    
-    if S.MLerroradjust
-        r_LM = r(1:n_LM);
-        r_HM = r(n_LM+1:end);
-        chi2by2 = 0.5*(n_LM*log(r_LM'*r_LM) +...
-                      n_HM*log(r_HM'*r_HM));  
+    rms = 1.0;
+    if (isfield(S, 'sd_LM') && isfield(S, 'sd_HM'))
+        n = n_LM + n_HM;
+        chi2by2 = 0.5*(norm(r_LM./S.sd_LM(idx_LM))^2 + ... 
+                       norm(r_HM./S.sd_HM(idx_HM))^2);
+        rms = 2*chi2by2/n;
     end
-    
+           
+    if S.MLerroradjust
+        r_LM = r_LM./abs(S.d_LM(idx_LM));
+        r_HM = r_HM./abs(S.d_HM(idx_HM));
+        chi2by2 = 0.5*(n_LM*log(r_LM'*r_LM) +...
+                       n_HM*log(r_HM'*r_HM));
+    end              
+                  
     misfit = [chi2by2 rms];
 else
     misfit = [0 0];
