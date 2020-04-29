e92cd45202147d48b4205db00fcceaf366d46685
Anand Ray <a2ray@ucsd.edu>
2020-04-30 00:01:52 +1000

    fname example wrong

--- AEM_examples/case01_ML/Step3_plot.m
+++ AEM_examples/case01_ML/Step3_plot.m
@@ -1,7 +1,7 @@
 % convergence stats
 plot_convergence_PT_RJMCMC('Test_SynthData/Test_synthData')
 subplot(421); ylim([0, 3])
-load('Test_SynthData/Test_SynthData_PT_RJMCMC_6.mat')
+load('Test_SynthData/Test_SynthData_PT_RJMCMC_4.mat')
 
 % posterior ensemble
 nBurnin = 50000; nThin = 1;
@@ -9,17 +9,17 @@ nBurnin = 50000; nThin = 1;
     plot_RJMCMC(1,s_ll,k_ll ,nBurnin,nThin,1,100,'isotropic','normalize','NologPDF',S_ll);
 
 % data fit for last model
+firstEmpty = find(k_ll==0,1);
+if (isempty(firstEmpty))
+    firstEmpty = length(k_ll)+1;
+end 
+s_ll = s_ll(1:firstEmpty-1);
 Bz = get_field(S_ll, S_ll.ReferenceModel);
 BzLast = get_field(S_ll, s_ll{end});
 
 figure()
-loglog(S_ll.times,abs(S_ll.data),'ro');
-hold on
-loglog(S_ll.times,abs(Bz),'k-','linewidth',1);
-loglog(S_ll.times,abs(BzLast),'b-','linewidth',1);
-grid()
-ylabel('dBz/dt (V/Am^4)')
-xlabel('Time (s)')
-set(gca,'fontsize',14)
-legend('Data', 'True model', 'Last model')
-title('Data fit')
+loglog(S.LM_times,abs(BzLast(1:nLM)),'k-','linewidth',1); hold on
+loglog(S.HM_times,abs(BzLast(nLM+1:end)),'k-','linewidth',1, 'HandleVisibility','off'); 
+loglog(S.LM_times,abs(Bz(1:nLM)),'r-','linewidth',1); 
+loglog(S.HM_times,abs(Bz(nLM+1:end)),'r-','linewidth',1, 'HandleVisibility','off'); 
+errorbar([S.LM_times; S.HM_times], [S.d_LM; S.d_HM], [S.sd_LM; S.sd_HM], '.'); 
